17be9e4eae46845e7463d4fe0f6e17cf8cc48ea6
Jinesh Prajapati <108856681+JineshPrajapati@users.noreply.github.com>
2024-05-22 11:54:07 +0530

    Update Dynamic_UpdateTrigger.sql

--- SQL/Triggeers/Dynamic_UpdateTrigger.sql
+++ SQL/Triggeers/Dynamic_UpdateTrigger.sql
@@ -4,7 +4,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER TRIGGER [dbo].[trgEmployeeDynamicAudit]
+Create TRIGGER [dbo].[trgEmployeeDynamicAudit]
 ON [dbo].[TABLENAME] -- update table name with your table name
 AFTER UPDATE
 AS
