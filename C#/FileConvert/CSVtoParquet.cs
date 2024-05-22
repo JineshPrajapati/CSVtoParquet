fbe57289298ae71b109fb9a0c3c7a70efb911184
Jinesh Prajapati <108856681+JineshPrajapati@users.noreply.github.com>
2024-05-22 11:14:40 +0530

    Update CSVtoParquet.cs

--- C#/FileConvert/CSVtoParquet.cs
+++ C#/FileConvert/CSVtoParquet.cs
@@ -1,1 +1,41 @@
+﻿using ChoETL;
+using System.Data;
 
+    //  for write parquet format  -->> install ChoETL  using this console command in terminal:  install-package ChoETL.Parquet
+    // *** Note *** : use this link to open Parquet File   https://www.parquet-viewer.com/#parquet-online
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Start time: " + DateTime.Now);
+
+        // convert CSV to .Net Datatable
+        DataTable dt = new DataTable();
+        using (StreamReader sr = new StreamReader(@"---CSV FILE PATH----"))
+        {
+            string[] headers = sr.ReadLine().Split(',');
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+            while (!sr.EndOfStream)
+            {
+                string[] rows = sr.ReadLine().Split(',');
+                DataRow dr = dt.NewRow();
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dr[i] = rows[i];
+                }
+                dt.Rows.Add(dr);
+            }
+        }
+
+        //write datatble to PARQUET file format
+        using (var w = new ChoParquetWriter("--- OUTPUT file path MUST be in .parquet format like hello.parquet) ---"))
+        {
+            w.Write(dt);
+        }
+        Console.WriteLine("End Time: "+DateTime.Now);
+    }
+}
